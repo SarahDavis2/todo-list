cecb663a02bbf8be8e201251fe7dadeb7cc758c5
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-16 15:59:24 -0500

    feat: Task class

--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ import "../node_modules/modern-normalize/modern-normalize.css";
 import "./styles/reset.css";
 import "./styles/styles.css";
 import { greeting } from "./scripts/greeting.js";
+import { Items } from "./scripts/Items.js";
+import { Project } from "./scripts/Project.js";
+import { Task } from "./scripts/Task.js";
 
 if (process.env.NODE_ENV !== 'production') {
    console.log('Looks like we are in development mode!');
 }
 
+localStorage.clear();
+
+// const items = new Items();
+// const test = new Project();
+// const test2 = new Project();
+// test._setTitle("TEST");
+// items._addItem(test);
+// items._addItem(test2);
+// items._storeItem(test);
+
+
+// console.log(items.getItems());
+// console.log(items.getStorageItem(test.id))
+
+// const task = new Task();
+// task.setDueDate(2020, 1, 31); // wont work because private
+// console.log(task.getDueDate());
 console.log(greeting);
+
+
+
+
+
+
---
+++ src/scripts/Task.js
@@ -0,0 +1,26 @@
+import { Project } from "./Project.js";
+import { format } from "date-fns";
+
+class Task extends Project {
+    #priority = null;
+    #dueDate = null;
+    #notes = null;
+    #isDone = false;
+
+    constructor() {
+        super();
+    }
+    #setPriority(setStatus) {
+        this.#priority = setStatus;
+    }
+    #setDueDate(year, month, day) {
+        this.#dueDate = format(new Date(year, month, day), "yyyy-MM-dd");
+    }
+    #setNote(setNote) {
+        this.#notes = setNote;
+    }
+    #isDone(setBool) {
+        this.#isDone = setBool;
+    }
+}
+export { Task };
